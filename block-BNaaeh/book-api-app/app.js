144056acac99a7d4780620094cd578339c9c0ad1
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-10-11 09:55:47 +0530

    update

--- block-BNaaeh/book-api-app/app.js
+++ block-BNaaeh/book-api-app/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var v1booksRouter = require('./routes/v1books');
+var v2booksRouter = require('./routes/v2books');
 
 var app = express();
 
@@ -21,14 +23,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1/books', v1booksRouter);
+app.use('/api/v2/books', v2booksRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
